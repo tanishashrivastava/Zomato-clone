02da9876906420d7a0a5f8144237c54c8b8e6dd6
Tanisha Shrivastava <silky.shrivastava08@gmail.com>
2021-10-16 22:18:40 +0530

    Food , Menu, Restaurant reviews, User API

---
+++ API/Menu/index.js
@@ -0,0 +1,29 @@
+//Libraries
+import express from "express";
+
+//Database Model
+import { MenuModel } from "../../database/allModels";
+
+const Router = express.Router();
+
+/*
+Route            /list
+Des              Get the list of menu based on id
+Params           _id
+Access           Public
+Method           GET
+*/
+
+Router.get("/list/:_id", async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const menus = await MenuModel.findOne(_id);
+
+        return res.json({ menus });
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
