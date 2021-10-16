230ccc99b852d49734463816df400d2498fab1fa
Tanisha Shrivastava <silky.shrivastava08@gmail.com>
2021-10-16 22:22:54 +0530

    aws connection, Image APi, adding get image feature in menu api, Route config for user auth Route config for user auth Order API

--- API/Menu/index.js
+++ API/Menu/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 
 //Database Model
-import { MenuModel } from "../../database/allModels";
+import { MenuModel, ImageModel } from "../../database/allModels";
 
 const Router = express.Router();
 
@@ -21,6 +21,25 @@ Router.get("/list/:_id", async (req, res) => {
 
         return res.json({ menus });
 
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+Route            /image
+Des              get menu image based on id
+Params           _id
+Access           Public
+Method           GET
+*/
+
+Router.get("/image/:_id", async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const menus = await ImageModel.findOne(_id);
+
+        return res.json({ menus });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
